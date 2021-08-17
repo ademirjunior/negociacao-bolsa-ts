29ebf4a9f91cc593c9e95d09e65f5faa7ab5c301
ademiralcojr <ademir@getinfo.tech>
2021-08-17 16:04:41 -0300

    Verificação de null e tratar de script injetado

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,15 +1,28 @@
 export abstract class View<T> {
 
     protected elemento: HTMLElement;
+    private escapar = false;
 
-    constructor(selector: string){
-        this.elemento = document.querySelector(selector);
+    constructor(selector: string, escapar?: boolean){
+        const elemento = document.querySelector(selector);
+        if (elemento) {
+            this.elemento = elemento as HTMLElement;
+        } else {
+            throw Error (`Seletor ${selector} não existe no DOM`)
+        }
+
+        if(escapar){
+            this.escapar = escapar;
+        }
     }
 
     protected abstract template(model: T): string;
 
     public update(model: T): void {
-        const template = this.template(model);
+        let template = this.template(model);
+        if (this.escapar) {
+            template = template.replace(/<script>[\s\S]*?<\/script>/,'');
+        }
         this.elemento.innerHTML = template;
     }
 }
