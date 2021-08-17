ee9f14b594f95a9451217d7413be50a95c951825
ademiralcojr <ademir@getinfo.tech>
2021-08-17 16:03:38 -0300

    Metodo estático

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -21,4 +21,12 @@ export class Negociacao {
     get volume(): number {
         return this._quantidade * this._valor;
     }
+
+    public static criaDe(dataString: string, quantidadeString: string, valorString: string): Negociacao {
+        const exp = /-/g;
+        const data = new Date(dataString.replace(exp, ','));
+        const quantidade = parseInt(quantidadeString);
+        const valor = parseFloat(valorString);
+        return new Negociacao(data, valor, quantidade);
+    }
 }
