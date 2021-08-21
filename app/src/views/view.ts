98e21c9d3f738ac8fb5c9a04f000f0894c1f6aa6
Ademir Junior <ademiralcojr@gmail.com>
2021-08-21 17:02:14 -0300

    Remoçaõ do escapar

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,33 +1,20 @@
-import { inspect } from "../decorators/inspect.js";
-import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
-
 export abstract class View<T> {
 
     protected elemento: HTMLElement;
-    private escapar = false;
 
-    constructor(selector: string, escapar?: boolean){
+    constructor(selector: string){
         const elemento = document.querySelector(selector);
         if (elemento) {
             this.elemento = elemento as HTMLElement;
         } else {
             throw Error (`Seletor ${selector} não existe no DOM`)
         }
-
-        if(escapar){
-            this.escapar = escapar;
-        }
     }
 
     protected abstract template(model: T): string;
    
-    @inspect
-    @logarTempoDeExecucao()
     public update(model: T): void {
         let template = this.template(model);
-        if (this.escapar) {
-            template = template.replace(/<script>[\s\S]*?<\/script>/,'');
-        }
         this.elemento.innerHTML = template;
     }
 }
